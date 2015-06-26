5af813fcb58365fc8782ab9103176d2a9d2a4cd6
arvinddhandapani <arvind.mib@gmail.com>
2015-06-26 19:44:39 +0530

    initial Commit

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+/*mysql_connect('localhost','root','') or die("could not connect ");
+mysql_select_db('mis');
+mysql_connect('localhost','root','') or die("could not connect ");
+mysql_select_db('abm');
+mysql_connect('localhost','mediain','media@321') or die("could not connect ");
+mysql_select_db('mediain_abm_new');
+mysql_connect('localhost','root','') or die("could not connect ");
+mysql_select_db('ilayaraaja');SS0IH_aTAktO*/
+//mysql_connect('localhost','filepark_raaja','&0_~[bpV-4mB') or die("could not connectt ");
+mysql_connect('localhost','raagas_raagam','Welcome123#$') or die("could not connectt ");
+//mysql_select_db('raagas_raagam');
+?>
---
+++ include/Config_li.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'raagas_raagam');
+define('DB_PASSWORD', 'Welcome123#$');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'raagas_16raagas');
+ 
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+?>
